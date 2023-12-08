4c857d32ab0c4794b3eb5fd86bb29c6072d11b94
Samuel Sfeir <samuel1808@hotmail.com>
2023-12-08 11:47:52 -0300

    adiciona requisito um e types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Login from './components/Login';
+
 function App() {
+  const handleLoginSuccess = () => {
+    // Lógica para redirecionar ou realizar outras ações após o login bem-sucedido
+    console.log('Login bem-sucedido! Redirecionando...');
+  };
+
   return (
-    <p>Trybetuness</p>
+    <Routes>
+      <Route index element={ <Login onLoginSuccess={ handleLoginSuccess } /> } />
+    </Routes>
   );
 }
 
---
+++ src/components/Login.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+
+type LoginProps = {
+  onLoginSuccess: () => void;
+};
+
+function Login({ onLoginSuccess }: LoginProps) {
+  // Cria o estado para controlar o valor do input, validacoes e loading
+  const [username, setUsername] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  // Função para lidar com o envio do formulário
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+      await createUser({ name: username });
+
+      // Chama a função de callback para lidar com o sucesso do login
+      onLoginSuccess();
+
+      // Navegar para a rota /search diretamente, sem esperar
+      navigate('/search');
+    } catch (error) {
+      console.error('Erro ao criar usuário:', error);
+      setIsLoading(false);
+      // Lidar com erros, se necessário
+    }
+  };
+
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    setUsername(inputValue);
+
+    // Habilitar o botão apenas se o nome tiver 3 ou mais caracteres
+    setIsButtonDisabled(inputValue.length < 3);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={ handleSubmit }>
+        {/* Input para o nome de usuário */}
+        <label>
+          Nome de usuário:
+          <input
+            type="text"
+            value={ username }
+            data-testid="login-name-input"
+            onChange={ handleUsernameChange }
+          />
+        </label>
+
+        {isLoading ? (
+          <p>Carregando...</p>
+        ) : (
+          <button
+            type="submit"
+            disabled={ isButtonDisabled }
+            data-testid="login-submit-button"
+          >
+            Entrar
+          </button>
+        )}
+      </form>
+    </div>
+  );
+}
+
+export default Login;
